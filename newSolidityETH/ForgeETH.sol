d1437e49df729f1c72eec2015014f161ce696582
Untouchable2k <dottage@gmail.com>
2022-09-25 22:30:51 -0500

    Update ForgeETH.sol

--- newSolidityETH/ForgeETH.sol
+++ newSolidityETH/ForgeETH.sol
@@ -363,7 +363,7 @@ function setDiv(uint divz, address token, amt) public{
 
 	require( (amt > ownerAmt[token] || amt > IERC20(token).balanceOf(address(this)) ) && (amt <= amountPerOwner[msg.sender][token]), "Must donate more than balance or last big send.");
 	amountPerOwner[msg.sender][token]  = amtPerOwner[msg.sender][token] - amt;
-	ownerAmt = amt;
+	ownerAmt[token] = amt;
 	require( divz >= 2000  && divz <= 5000000, "Must be within 2000 - 5000000");
 	divide[token] = divz;
 	}
@@ -672,6 +672,10 @@ function zinit(address AuctionAddress2, address LPGuild2, address _ZeroXBTCAddre
 						totalOwed = (TotalOwned * totalOwed).div(100000000 * whatDiv(ExtraFunds[x]) * 2 );
 				    }
 			    IERC20(ExtraFunds[x]).transfer(MintTo[x], totalOwed);
+			    if(ownerAmt[token] > totalOwed )
+			    	ownerAmt[token] = ownerAmt[token] - totalOwed;
+			    }else{
+			       	ownerAmt[token] = ownerAmt[token] - ownerAmt[token];
 			    }
             }
         }
