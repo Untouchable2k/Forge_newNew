b6ac487dfdcc12ca3ff14a25960fd8c08e8a943a
Untouchable2k <dottage@gmail.com>
2022-11-19 23:16:27 -0600

    Create 0xbtcMock.sol

---
+++ Goerli/0xbtcMock.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+
+contract a0xbtcMock is ERC20 {
+	constructor() public ERC20("Mock 0xbtc5", "M0xBTC", 8) {
+        
+    }
+    function withdrawToken(uint256 _amount) external {
+        // transfer the token from address of this contract
+        // to address of the user (executing the withdrawToken() function)
+                _mint(msg.sender, _amount);
+    }
+}
