44cdab9babbf694c8a0eb08f82f97fe753a34f2d
Untouchable2k <dottage@gmail.com>
2022-12-02 11:01:14 -0600

    Update Staking.sol

--- Goerli/Staking.sol
+++ Goerli/Staking.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2022-11-19
-*/
-
 // Arbitrum Bitcoin and Staking - Staking Contract #1
 //
 // Balancer Liquidity Pool ABAS / ETH Staking Rewards contract
